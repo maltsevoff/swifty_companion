2bf9de6214b3987de3ee9088986482b21185128b
maltsevoff <alexmalczev@gmail.com>
2019-07-13 13:52:58 +0300

    start

---
+++ companion/Model.swift
@@ -0,0 +1,17 @@
+//
+//  Model.swift
+//  companion
+//
+//  Created by Alex Maltsev on 7/9/19.
+//  Copyright © 2019 Alex Maltsev. All rights reserved.
+//
+
+import Foundation
+
+struct MyData {
+	let uid = "b6963484b1e51cb154c83ac53655924179d717b55ddfe15caeda33e135bd73a2"
+	let secretKey = "b1595e069ffb6842c092f0e7befb654611d82b640e9ff97878c04b8e5d3e7522"
+	let redicetUrl = "CompanionApp%3A%2F%2FCompanionApp"
+	let site = "https://api.intra.42.fr/"
+	var token: String = ""
+}
---
+++ companion/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  companion
+//
+//  Created by Alex Maltsev on 7/9/19.
+//  Copyright © 2019 Alex Maltsev. All rights reserved.
+//
+
+import UIKit
+import SafariServices
+import AuthenticationServices
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+	
+}
+
