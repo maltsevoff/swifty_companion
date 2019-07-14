4e5f972fa4120e1d04ef80400a02d4a3eef8d41d
maltsevoff <alexmalczev@gmail.com>
2019-07-14 15:26:31 +0300

    works first request

--- companion/Model.swift
+++ companion/Model.swift
@@ -11,7 +11,7 @@ import Foundation
 struct MyData {
 	let uid = "b6963484b1e51cb154c83ac53655924179d717b55ddfe15caeda33e135bd73a2"
 	let secretKey = "b1595e069ffb6842c092f0e7befb654611d82b640e9ff97878c04b8e5d3e7522"
-	let redicetUrl = "CompanionApp%3A%2F%2FCompanionApp"
+	let redirectUrl = "CompanionApp%3A%2F%2FCompanionApp"
 	let site = "https://api.intra.42.fr/"
 	var token: String = ""
 }
--- companion/ViewController.swift
+++ companion/ViewController.swift
@@ -7,16 +7,36 @@
 //
 
 import UIKit
-import SafariServices
-import AuthenticationServices
+import Alamofire
 
 class ViewController: UIViewController {
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
+		authUsingIntra()
 		// Do any additional setup after loading the view.
+		
 	}
 
+	func authUsingIntra () {
+		let myData = MyData.init()
+		let reqUrl = myData.site +  "oauth/authorize?client_id=\(myData.uid)&redirect_uri=\(myData.redirectUrl)&response_type=code"
+//		let reqUrl = "https://api.intra.42.fr/oauth/authorize?client_id=\(myData.uid)&redirect_uri=\(myData.redirectUrl)&response_type=code"
+		print(myData.uid, myData.redirectUrl, myData.secretKey)
+		
+//		request(reqUrl, method: .get, parameters: nil, encoding: URLEncoding.default, headers: nil)
+//			.validate().responseJSON { response in
+//				if response.result.value != nil {
+//					print(response)
+//				} else {
+//					print("Error tut")
+//				}
+		
+		request(reqUrl, method: .get).authenticate(user: myData.uid, password: myData.secretKey)
+			.responseString { response in
+				print(response)
+		}
+	}
 	
 }
 
